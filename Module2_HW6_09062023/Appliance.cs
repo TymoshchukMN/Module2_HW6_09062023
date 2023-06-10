5b7435e755a50a88b9355cbe6aadfe3c7ac4c134
Tymoshchuk Maksym <MTimoshchukmn.ua@gmail.com>
2023-06-10 14:17:32 +0300

    added base class

---
+++ Module2_HW6_09062023/Appliance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Module2_HW6_09062023
+{
+    public class Appliance
+    {
+        private int _voltage;
+
+        private int _energyConsumption;
+
+        private string _manufacturer;
+
+        public string Manufacturer
+        {
+            get { return _manufacturer; }
+            set { _manufacturer = value; }
+        }
+
+        public int EnergyConsumption
+        {
+            get { return _energyConsumption; }
+            set { _energyConsumption = value; }
+        }
+
+        public int Voltage
+        {
+            get { return _voltage; }
+            set { _voltage = value; }
+        }
+
+    }
+}
---
+++ Module2_HW6_09062023/Starter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_HW6_09062023
+{
+    public static class Starter
+    {
+        public static void Run()
+        {
+        }
+    }
+}
