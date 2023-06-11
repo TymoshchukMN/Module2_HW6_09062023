fe5ef49b328c5bcc31c1f8887cd5454dc27b7e8f
Tymoshchuk Maksym <MTimoshchukmn.ua@gmail.com>
2023-06-11 15:17:57 +0300

    removed None vol from enums

---
+++ Module2_HW6_09062023/PropRand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_HW6_09062023
+{
+    internal class PropRand
+    {
+    }
+}
